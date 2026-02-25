2f99600c2c2ad38c3e328ce72388137b0925e592
dinhlam728 <dinhlam728@gmail.com>
2026-02-25 13:13:49 +0700

    order and product

--- backend/src/main/java/com/tallt/marketplace/entity/Order.java
+++ backend/src/main/java/com/tallt/marketplace/entity/Order.java
@@ -1,18 +1,13 @@
 package com.tallt.marketplace.entity;
-<<<<<<< HEAD
 
 import jakarta.persistence.*;
 import lombok.Data;
 
-=======
-import jakarta.persistence.*;
->>>>>>> a2a09c3a7a25716178ee1a006acc5464266ec17e
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "Orders")
-<<<<<<< HEAD
 @Data
 public class Order {
     @Id
@@ -56,20 +51,3 @@ public class Order {
     @Column(name = "CreatedAt", updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 }
-=======
-public class Order {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer orderID;
-
-    private BigDecimal totalAmount;
-    private String paymentStatus;
-    private LocalDateTime createdAt;
-
-    @ManyToOne
-    @JoinColumn(name = "ProductID")
-    private Product product;
-}
-
->>>>>>> a2a09c3a7a25716178ee1a006acc5464266ec17e
