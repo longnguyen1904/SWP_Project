32299b3c2a5dddb9f769c59310d12ec1a233baf5
dinhlam728 <dinhlam728@gmail.com>
2026-03-02 12:16:17 +0700

    update code

---
+++ backend/src/main/java/com/tallt/marketplace/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.tallt.marketplace.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // <-- SỬA DÒNG NÀY
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
