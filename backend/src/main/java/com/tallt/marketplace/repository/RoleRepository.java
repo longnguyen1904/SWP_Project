83b53f043eec82293f622922851fce63e67ae266
dinhlam728 <dinhlam728@gmail.com>
2026-03-02 12:25:26 +0700

    Revert "update code"
    This reverts commit 32299b3c2a5dddb9f769c59310d12ec1a233baf5.

--- backend/src/main/java/com/tallt/marketplace/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tallt.marketplace.repository;
-
-import com.tallt.marketplace.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
