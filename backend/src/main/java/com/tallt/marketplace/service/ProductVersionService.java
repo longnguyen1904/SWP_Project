72e350b2b0ba2af2f6d43d6f37ae767da4d04f99
dinhlam728 <dinhlam728@gmail.com>
2026-03-28 12:20:40 +0700

    Update ProductVersionService.java

--- backend/src/main/java/com/tallt/marketplace/service/ProductVersionService.java
+++ backend/src/main/java/com/tallt/marketplace/service/ProductVersionService.java
@@ -78,10 +78,6 @@ public class ProductVersionService {
         version.setReleaseNotes(request.getReleaseNotes());
         productVersionRepository.save(version);
 
-        if (product.getStatus() == Product.ProductStatus.APPROVED) {
-            notifyBuyersOfNewVersion(product, version);
-        }
-
         return toResponse(version);
     }
 
